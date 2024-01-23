cd5f3727a9b34f4a6204cf877f7c392e9408d757
gonzalez-lourdnathaniel <gonzalez.lourdnathaniel@gmail.com>
2024-01-23 08:54:22 +0800

    Created Quiz Generator page

---
+++ src/pages/QuizGenerator.jsx
@@ -0,0 +1,11 @@
+import "../assets/css/home.css";
+import { Container } from "react-bootstrap";
+
+export default function QuizGenerator() {
+    
+    return (
+        <Container fluid id="page-home" className="page page-home">
+            <h1 className="title">This is the Quiz Generator Page</h1>
+        </Container>
+    );
+};
