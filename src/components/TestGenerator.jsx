737e6f8608ebb14cb8b5e55701a8d5190389b4e5
gonzalez-lourdnathaniel <gonzalez.lourdnathaniel@gmail.com>
2024-03-20 21:13:02 +0800

    Allowed dynamic changed of subtopics

--- src/components/TestGenerator.jsx
+++ src/components/TestGenerator.jsx
@@ -1,18 +1,76 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import "../assets/css/quizGenerator.css";
 import "../assets/css/testGenerator.css";
-import Enumeration from "./questionTypes/Enumeration";
-import Essay from "./questionTypes/Essay";
-import MultipleAnswer from "./questionTypes/MultipleAnswer";
-import MultipleChoice from "./questionTypes/MultipleChoice";
-import ShortAnswer from "./questionTypes/ShortAnswer";
-import ProblemSolving from "./questionTypes/ProblemSolving";
 import { Button, Card, Container, Dropdown, Form, InputGroup, Stack } from "react-bootstrap";
 
 export default function TestGenerator({appTheme}) {
     const [selectedTestType, setSelectedTestType] = useState("");
     const [selectedSubject, setSelectedSubject] = useState("");
-    const [selectedChapter, setSelectedChapter] = useState("");
+    const [selectedChapter, setSelectedChapter] = useState('');
+    const [availableSubjects, setAvailableSubjects] = useState([]);
+    const [availableTopics, setAvailableTopics] = useState([]);
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}/question/getSubjects`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(async (res) => {
+            const responseJson = await res.json();
+
+            setAvailableSubjects(responseJson.map((subject, index) => {
+                return (
+                    <Dropdown.Item key={index} onClick={() => setSelectedSubject(subject)}>{subject}</Dropdown.Item>
+                );
+            }));
+        });
+    }, []);
+
+    useEffect(() => {
+        if (selectedChapter !== '' && selectedSubject !== '') {
+            let chapter;
+            if (selectedChapter === 'Chapter I') {
+                chapter = 1;
+            }
+            else if (selectedChapter === 'Chapter II') {
+                chapter = 2;
+            }
+            else if (selectedChapter === 'Chapter III') {
+                chapter = 3;
+            }
+            else {
+                chapter = 4
+            }
+
+            fetch(`${process.env.REACT_APP_API_URL}/question/getSubtopics`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    subject: selectedSubject,
+                    chapter: chapter
+                })
+            })
+            .then(async (res) => {
+                const responseJson = await res.json();
+                console.log(responseJson);
+    
+                if (responseJson.length === 0) {
+                    setAvailableTopics([]);
+                }
+                else {
+                    setAvailableTopics(responseJson.map((topic, index) => {
+                        return (
+                            <Form.Check key={index} type="checkbox" label={topic} name={topic} />
+                        );
+                    }));
+                }
+            });
+        }
+    }, [selectedSubject, selectedChapter]);
 
     return (
         <Fragment>
@@ -38,19 +96,19 @@ export default function TestGenerator({appTheme}) {
                 <Card.Body>
                     <textarea row="4" id="instruction" className={`bg-${appTheme} txtBox-instruction w-100 p-2 mb-2`} />
 
-                    <Stack direction="horizontal" gap={3} className="mb-3">
+                    <Stack direction="horizontal" gap={3} className="mb-3 flex-wrap">
                         <Dropdown>
                             <Dropdown.Toggle variant="secondary" id="testType" className="dropdown-selector">
                                 {selectedSubject === '' ? 'Select test type...' : selectedSubject}
                             </Dropdown.Toggle>
 
                             <Dropdown.Menu>
-                                <Dropdown.Item onClick={() => setSelectedSubject("Mathematics in the Modern World")}>Mathematics in the Modern World</Dropdown.Item>
+                                {availableSubjects}
                             </Dropdown.Menu>
                         </Dropdown>
                         <Dropdown>
                             <Dropdown.Toggle variant="secondary" id="testType" className="dropdown-selector">
-                                {selectedChapter === '' ? 'Select test type...' : selectedChapter}
+                                {selectedChapter === '' ? 'Select chapter...' : selectedChapter}
                             </Dropdown.Toggle>
 
                             <Dropdown.Menu>
@@ -66,20 +124,12 @@ export default function TestGenerator({appTheme}) {
                         <h5 className="subtitle fs-6">Sub-topic:</h5>
                         
                         <Container fluid className="checkbox-list m-0 w-100">
-                            <Form.Check type="checkbox" id="option1" label="Option 1" name="option 1" />
-                            <Form.Check type="checkbox" id="option2" label="Option 2" name="option 2" />
-                            <Form.Check type="checkbox" id="option3" label="Option 3" name="option 3" />
-                            <Form.Check type="checkbox" id="option4" label="Option 4" name="option 4" />
-                            <Form.Check type="checkbox" id="option5" label="Option 5" name="option 5" />
-                            <Form.Check type="checkbox" id="option6" label="Option 6" name="option 6" />
-                            <Form.Check type="checkbox" id="option7" label="Option 7" name="option 7" />
-                            <Form.Check type="checkbox" id="option8" label="Option 8" name="option 8" />
-                            <Form.Check type="checkbox" id="option9" label="Option 9" name="option 9" />
+                            {availableTopics}
                         </Container>
                     </Stack>
                 </Card.Body>
 
-                <Card.Footer className="d-flex align-items-center justify-content-between">
+                <Card.Footer className="gap-3">
                     <InputGroup>
                         <InputGroup.Text>Number of item(s)</InputGroup.Text>
                         <Form.Control type="number" />
@@ -95,12 +145,6 @@ export default function TestGenerator({appTheme}) {
             </Card>
             
             <Stack direction="vertical" gap={3} className="my-3">
-                <Enumeration appTheme={appTheme} />
-                <Essay appTheme={appTheme} />
-                <MultipleAnswer appTheme={appTheme} />
-                <MultipleChoice appTheme={appTheme} />
-                <ShortAnswer appTheme={appTheme} />
-                <ProblemSolving appTheme={appTheme} />
             </Stack>
         </Fragment>
     );
